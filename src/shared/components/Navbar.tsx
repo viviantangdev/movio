c2e7b8a1016de6a7e1cd165dd9e19e5a5b94b618
Vivian Tang <viviantang.dev@gmail.com>
2025-10-29 11:37:13 +0100

    Responsive navbar

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -13,19 +13,21 @@ const Links: NavbarLink[] = [
 
 const Navbar = () => {
   return (
-    <header className='w-full p-5 flex justify-between items-center'>
-      <Link to='/'>
-        <Logo />
-      </Link>
-      <ul className='flex gap-6'>
-        {Links.map((li, index) => (
-          <Link to={li.linkTo} key={index}>
-            <li className='underline underline-offset-4 decoration-1 decoration-transparent transition-smooth hover:decoration-emerald-400'>
-              {li.navName}
-            </li>
-          </Link>
-        ))}
-      </ul>
+    <header className='flex justify-between items-center w-[90%] md:w-[80%] 2xl:w-[50%] max-w-7xl mx-auto py-7'>
+
+        <Link to='/'>
+          <Logo />
+        </Link>
+        <ul className='flex gap-6'>
+          {Links.map((li, index) => (
+            <Link to={li.linkTo} key={index}>
+              <li className='underline underline-offset-4 decoration-1 decoration-transparent transition-smooth hover:decoration-emerald-400'>
+                {li.navName}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      
     </header>
   );
 };
